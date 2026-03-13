cc0da2155d8ceb52597f46d6c36fa87d8bd3c7d2
valfrides07 <freitascoc687@gmail.com>
2026-03-13 15:12:26 -0300

    Padronizando nomes de pastas e imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
-import "./App.css";
+import Header from "./components/Header/index"; // ou apenas "./components/Header"
+import Hero from "./components/Hero/index";
+import Footer from "./components/Footer/index";
 
-import Header from "./components/Header";
-import Hero from "./components/hero";
-import Footer from "./components/footer";
+import Projetos from "./Pages/Projetos/index";
+import Contatos from "./Pages/Contatos/index";
+import SobreMim from "./Pages/SobreMim/index";
+import Direitos from "./Pages/DireitosAutorais/index";
 
-import Projetos from "./Pages/projects";
-import Contatos from "./Pages/contatos";
-import SobreMim from "./Pages/sobreMim";
-import Direitos from "./Pages/direitosAutorais";
-
-// Import de rotas para os <links> serem usados
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    
-      {/* o header fica fora de Routes para rendenrizar em todas as paginas */}
       <Header />
-
       <div>
         <Routes>
-          {/* Pagina home */}
           <Route path="/" element={<Hero />} />
-
-          {/* Links para outras abas dentro do header */}
           <Route path="/Projetos" element={<Projetos />} />
-          <Route path="/Contato" element={<Contatos />} />
-          <Route path="/Sobre" element={<SobreMim />} />
-          
-          {/* Direitos autorais do footer */}
-          <Route path="/Direitos" element={<Direitos />} />"
-          
-          {/* Caso tente acessar um link que nao exista, ira mostrar essa mensagem na tela */}
-          <Route path="*" element={<h1>Pagina não encontrada.</h1>} />
+          <Route path="/Contatos" element={<Contatos />} />
+          <Route path="/SobreMim" element={<SobreMim />} />
+          <Route path="/DireitosAutorais" element={<Direitos />} />
         </Routes>
       </div>
-
       <Footer />
     </BrowserRouter>
   );
